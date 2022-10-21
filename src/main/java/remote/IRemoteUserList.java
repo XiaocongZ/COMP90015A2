3734c89110a99926773633fa8d836fe5746ba89d
Yukash Sivaraj <63547766+YSPlop@users.noreply.github.com>
2022-10-21 15:36:33 +1100

    comments

--- src/main/java/remote/IRemoteUserList.java
+++ src/main/java/remote/IRemoteUserList.java
@@ -6,12 +6,16 @@ import java.util.List;
 
 public interface IRemoteUserList extends Remote {
 
+    // adds user to the user list
     public void addUser(String userName) throws RemoteException;
 
+    // gets the list of users connected to the server as of now
     List<String> getUserNames() throws RemoteException;
 
+    // gets the manager of the server
     String getManagerName() throws RemoteException;
 
+    // only used when createWhiteboard, sets manager name
     void setManagerName(String managerName) throws RemoteException;
 
 }
